3658ef803bd967a91b2d176ba0cf6b83151c1399
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-07-05 01:20:50 +0200

    Sort technologies, make technology headers highlight items as well
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-const techHeaders = Array.from(document.querySelectorAll(".technologies-header li"));
-const $technologyItems = document.querySelectorAll(".technologies-items li");
-$technologyItems.forEach($item => {
-	$item.addEventListener("pointerenter", enableTechItem);
+const technologyHeaders = Array.from(document.querySelectorAll(".technologies-header li"));
+const technologyItems = Array.from(document.querySelectorAll(".technologies-items li"));
+technologyItems.forEach($item => {
+  $item.addEventListener("pointerenter", enableTechItem);
   $item.addEventListener("pointerleave", disableTechItem);
+});
+technologyHeaders.forEach($header => {
+  $header.addEventListener("pointerenter", enableTechHeader);
+  $header.addEventListener("pointerleave", disableTechHeader);
 });
 
 function disableTechItem() {
-  techHeaders.forEach(header => header.classList.remove("active"));
+  technologyHeaders.forEach(header => header.classList.remove("active"));
 }
+
 function enableTechItem(event) {
   const $item = event.target;
   Array.from($item.classList).forEach(cls => {
-    techHeaders.filter(header => header.classList.contains(cls)).forEach(header => header.classList.add("active"));
+    technologyHeaders
+      .filter(header => header.classList.contains(cls))
+      .forEach(header => header.classList.add("active"));
+  });
+}
+
+function disableTechHeader() {
+  technologyItems.forEach(header => header.classList.remove("active"));
+}
+
+function enableTechHeader(event) {
+  const $header = event.target;
+  Array.from($header.classList).forEach(cls => {
+    technologyItems.filter(item => item.classList.contains(cls)).forEach(item => item.classList.add("active"));
   });
 }
