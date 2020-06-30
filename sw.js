13e3c2d1485a865b90b4dba9faa56059a6f89409
Philip Saa <philip.saa@compose.us>
2020-06-30 08:38:32 +0000

    Auto-generated commit

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c5888c2cbceaa926a5d4.js"
+    "url": "webpack-runtime-e794a508581b2d2d190b.js"
   },
   {
     "url": "styles.b066d14b4eae69f29da1.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2cca276f86ed3dea2b4b4321891863a6"
+    "revision": "7e99254ea123c09ffbb617ccdb1460ef"
   },
   {
     "url": "manifest.webmanifest",
