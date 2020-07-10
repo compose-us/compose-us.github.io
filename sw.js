eef86c9ceea01112139ea59444a5531be2cf2b5c
Philip Saa <cowglow@gmail.com>
2020-07-10 13:15:58 +0000

    Auto-generated commit

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-67decc6f39093792ad63.js"
+    "url": "webpack-runtime-be45fb40dbf9f682d221.js"
   },
   {
     "url": "styles.b066d14b4eae69f29da1.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bc6c4304c1001f6c1fd3964cddf75e00"
+    "revision": "2d1a053b2d6be166c883d545145b4ed9"
   },
   {
     "url": "manifest.webmanifest",
